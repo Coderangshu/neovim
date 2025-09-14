571ae563a4c7bb14581e2702b151ff3a37823251
Angshuman Sengupta <senguptaangshuman17@gmail.com>
2025-09-15 00:43:28 +0530

    Fixed indentation problem

--- lua/setup_treesitter.lua
+++ lua/setup_treesitter.lua
@@ -1,6 +1,8 @@
 require("nvim-treesitter.configs").setup({
     ensure_installed = { "c", "cpp", "lua", "python", "javascript", "typescript", "bash", "json", "html", "css", "rust" },
     highlight = { enable = true },
-    indent = { enable = true },
+    indent = { enable = false,
+               disable = { "cpp", "javascript", "typescript" }  -- Disable indent for cpp and js/ts due to issues
+    },
     fold = { enable = true },
 })
