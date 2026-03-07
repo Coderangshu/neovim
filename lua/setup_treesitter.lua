183514e08a75df15b6324b294de24a8abc2f7ba7
Angshuman Sengupta <senguptaangshuman17@gmail.com>
2026-03-07 15:30:43 +0530

    treesitter config updated

--- lua/setup_treesitter.lua
+++ lua/setup_treesitter.lua
@@ -1,9 +1,9 @@
-require("nvim-treesitter.config").setup({
+require("nvim-treesitter.configs").setup({
     ensure_installed = { "c", "cpp", "lua", "python", "javascript", "typescript", "bash", "json", "html", "css", "rust" },
     highlight = { enable = true },
     indent = {
         enable = false,
-        disable = { "cpp", "javascript", "typescript" }        -- Disable indent for cpp and js/ts due to issues
+        disable = { "cpp", "javascript", "typescript" } -- Disable indent for cpp and js/ts due to issues
     },
     fold = { enable = true },
 })
