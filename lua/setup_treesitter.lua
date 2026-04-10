a32597e2d766a57e6c06b76ebba8498c47a24a85
Angshuman Sengupta <senguptaangshuman17@gmail.com>
2026-04-11 00:01:48 +0530

    Treesitter sync install before highlighting added

--- lua/setup_treesitter.lua
+++ lua/setup_treesitter.lua
@@ -1,9 +1,9 @@
 require("nvim-treesitter.configs").setup({
     ensure_installed = { "c", "cpp", "lua", "python", "javascript", "typescript", "bash", "json", "html", "css", "rust" },
+    sync_install = true,
     highlight = { enable = true },
     indent = {
         enable = false,
         disable = { "cpp", "javascript", "typescript" } -- Disable indent for cpp and js/ts due to issues
     },
-    fold = { enable = true },
 })
